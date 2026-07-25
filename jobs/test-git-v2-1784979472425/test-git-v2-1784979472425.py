2b374038ac5b6ad050d88c8f0ceef9f3693bd091
ThunderDomainAuthSession <ThunderDomainAuthSession>
2026-07-25 11:39:39 +0000

    Add test-git-v2-1784979472425 via Git V2 E2E test

--- jobs/test-git-v2-1784979472425/test-git-v2-1784979472425.py
+++ jobs/test-git-v2-1784979472425/test-git-v2-1784979472425.py
@@ -6,7 +6,7 @@ sc = SparkContext.getOrCreate()
 spark = SparkSession.builder.getOrCreate()
 
 # Script generated for node S3DataSource
-S3DataSource_1784979156481 = spark.read.format("csv") \
+S3DataSource_1784979496218 = spark.read.format("csv") \
     .option("inferschema", "true") \
     .option("multiLine", "true") \
     .option("header", "true") \
